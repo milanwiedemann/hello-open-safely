88b9da558989e2f5c685e87a959905524a3220b0
Milan Wiedemann <milan.wiedemann@gmail.com>
2021-10-18 20:10:36 +0100

    adding cells to python script so I can run line by line like jupyter notebook

--- analysis/report.py
+++ analysis/report.py
@@ -1,5 +1,10 @@
+# Python script to create first plot
+# Adding "Run Cell" functionality by starting new cell with #%%
+#%% Import packages
 import pandas as pd
-
-data = pd.read_csv("output/input.csv")
+#%% Load data
+data = pd.read_csv("../output/input.csv")
+#%% Create plot
 fig = data.age.plot.hist().get_figure()
-fig.savefig("output/descriptive.png")
+#%% Save figure
+fig.savefig("../output/descriptive.png")
