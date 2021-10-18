649c44d3d862f4f4b60ad3ad7b899482a0fac18b
Milan Wiedemann <milan.wiedemann@gmail.com>
2021-10-18 20:11:08 +0100

    add more vars to study definition

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,19 +1,37 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import (
+    StudyDefinition,
+    codelist,
+    codelist_from_csv,
+    combine_codelists,
+    filter_codes_by_category,
+    patients
+)
+
+# from codelists import *
+
+# define the study index date
+index_date = "2020-01-01"
 
 study = StudyDefinition(
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {"earliest": "1970-01-01", "latest": "today"},
         "rate": "uniform",
-        "incidence": 0.5,
+        "incidence": 0.2,
     },
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
     ),
-        age=patients.age_as_of(
-        "2019-09-01",
+    age=patients.age_as_of(
+        index_date,
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
         },
+    ),
+    sex=patients.sex(
+        return_expectations={
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+            "incidence": 1,
+        }
     ),
 )
